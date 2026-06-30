803fab6f3449d87fe8f6f92e65ab78af512c7229
Axel Cabral <axelfernandocabral@gmail.com>
2026-06-30 17:00:41 +0000

    Sysadmin: Agrego parte sysadmin para el script de continentes

---
+++ Punto_A/Grupal/Punto_A_Grupal.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Script para generar la estructura grupal de continentes del punto A
+
+# Directorios para generar la estructura de Ameria, Rol = sysadmin
+mkdir -p ~/Continentes/America/Peru/{Lima,Cusco,Arequipa} ~/Continentes/America/Colombia/{Bogotá,Medellin} ~/Continentes/America/Mexico/CiudadDeMexico
