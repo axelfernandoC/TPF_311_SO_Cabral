805de8e7cac5f508144fcdd1c18e7a50b7e8c4bb
Maximo Cabral <axelcabral@matesmv.com>
2026-06-30 16:48:37 +0000

    Agrego la estructura dev del punto A Individual

--- Punto_A/Individual/Punto_A_Individual.sh
+++ Punto_A/Individual/Punto_A_Individual.sh
@@ -17,3 +17,18 @@ mkdir -p ~/manager/Monitoreo/{Logs,Alertas,Metricas}
 
 touch ~/manager/Servicios/{Cache,Base_de_Datos,Web}/.gitkeep
 touch ~/manager/Monitoreo/{Logs,Alertas,Metricas}/.gitkeep
+
+echo "Directorios manager manager Listos!"
+
+# Directorios del rol dev
+
+mkdir -p ~/dev/Servicios/{Cache,Base_de_Datos,Web}
+mkdir -p ~/dev/Monitoreo/{Logs,Alertas,Metricas}
+
+touch ~/dev/Servicios/{Cache,Base_de_Datos,Web}/.gitkeep
+touch ~/dev/Monitoreo/{Logs,Alertas,Metricas}/.gitkeep
+
+echo "Directorios dev Listos!"
+
+echo "Estructura completa Lista!"
+
