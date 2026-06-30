ae3afea7cf877a04914535cbbbbea49da808b449
Fernando Cabral <axelcabral94@gmail.com>
2026-06-30 16:23:39 +0000

    Agrego la estructura manager del Punto A Individual

--- Punto_A/Individual/Punto_A_Individual.sh
+++ Punto_A/Individual/Punto_A_Individual.sh
@@ -9,3 +9,11 @@ touch ~/sysadmin/Servicios/{Cache,Base_de_Datos,Web}/.gitkeep
 touch ~/sysadmin/Monitoreo/{Logs,Alertas,Metricas}/.gitkeep
 
 echo "Directorios sysadmin ¡Listos! "
+
+# Directorios del rol manager
+
+mkdir -p ~/manager/Servicios/{Cache,Base_de_Datos,Web}
+mkdir -p ~/manager/Monitoreo/{Logs,Alertas,Metricas}
+
+touch ~/manager/Servicios/{Cache,Base_de_Datos,Web}/.gitkeep
+touch ~/manager/Monitoreo/{Logs,Alertas,Metricas}/.gitkeep
