1edeae89a1e02cc2435b26e478326c3c7d436590
Maximo Cabral <axelcabral@matesmv.com>
2026-06-30 17:28:40 +0000

    Dev: Agrego parte dev para el script continentes

--- Punto_A/Grupal/Punto_A_Grupal.sh
+++ Punto_A/Grupal/Punto_A_Grupal.sh
@@ -8,3 +8,7 @@ mkdir -p ~/Continentes/America/Peru/{Lima,Cusco,Arequipa} ~/Continentes/America/
 # Directorios para generar la estructura de Europa, Rol = manager
 
 mkdir -p ~/Continentes/Europa/España/{Madrid,Barcelona,Sevilla} ~/Continentes/Europa/SegundoFrancia/{Paris,Lyon} ~/Continentes/Europa/Italia/Roma
+
+# Directorios para generar la estructura de Asia, Rol = dev
+
+mkdir -p ~/Continentes/Asia/Japon/{Tokio,Osaka,Kioto} ~/Continentes/Asia/China/{Pekín,Shangái} ~/Continentes/Asia/India/Mumbai
